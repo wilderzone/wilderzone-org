ce97eab13dd6b145946308b87150d66c3c044df2
Giga <gigabyte5671@gmail.com>
2021-10-24 22:19:10 +1300

    Update updateDOM.js

--- player/updateDOM.js
+++ player/updateDOM.js
@@ -1,6 +1,7 @@
 function updateDOM(){
   document.querySelector("main").innerHTML = (
     Lightshard.blocks.h1(player_data.player_name) +
-    Lightshard.blocks.info("Rank XP: " + player_data.rank_xp)
+    Lightshard.blocks.info("Rank XP: " + player_data.rank_xp) +
+    Lightshard.blocks.info("Rank: " + xpToRank(player_data.rank_xp)["name"])
   );
 }
