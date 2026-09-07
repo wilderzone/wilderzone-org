09b87614b12b6ba2401f6a5382dfd464eed255a9
Giga <gigabyte5671@gmail.com>
2026-09-07 18:37:02 +1200

    Convert the T2 page to Nuxt

---
+++ app/pages/games/tribes-2/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import favicon from './favicon.gif';
+
+const redirect = 'https://www.tribesnext.com/';
+
+useHead({
+	title: 'Redirecting...',
+	link: [
+		{ rel: 'shortcut icon', href: favicon, type: 'image/gif' },
+	]
+});
+
+await navigateTo(redirect, { external: true });
+</script>
+
+<template>
+	<p>If you are not redirected automatically, follow this <a :href="redirect" rel="noreferrer">link</a>.</p>
+</template>
+
+<style scoped>
+</style>
