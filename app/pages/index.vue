b8c282f333df7935da9593f6e92f772b7fb2e87c
Giga <gigabyte5671@gmail.com>
2026-09-16 15:20:16 +1200

    Add an intro message to the landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,21 @@
 	<NavBar />
 	<PanoramaBar />
 	<main>
+		<div class="intro">
+			<p>Welcome to the Wilderzone.org restoration project.</p>
+			<p>We are in the process of restoring the <a href="https://web.archive.org/web/20160622130520/http://www.wilderzone.org/" target="_blank" rel="noreferrer">original Wilderzone.org website</a> that you may be familiar with, as well as bringing it up-to-date with the latest TA community content.</p>
+			<p>You can contribute to the project through <a href="https://github.com/wilderzone/wilderzone-org" target="_blank" rel="noreferrer">our GitHub repository</a>, or get in contact with us via the <a href="https://discord.gg/dd8JgzJ">TA Community Hub Discord server</a>.</p>
+			<p>Have a nice day!</p>
+		</div>
 	</main>
 	<Footer />
 </template>
+
+<style scoped>
+.intro {
+	width: 100%;
+	max-width: var(--content-width);
+	margin: auto;
+	padding: var(--gap-xl) var(--gap-m);
+}
+</style>
